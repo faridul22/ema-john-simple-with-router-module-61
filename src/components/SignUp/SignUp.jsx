3cc1a91fd318446ef2930797a73fb5c2c3642be2
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-01-29 22:21:12 +0600

    Basic project setUp done

---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignUp.css'
+
+const SignUp = () => {
+    return (
+        <div>
+            <h3>This is sign up page</h3>
+        </div>
+    );
+};
+
+export default SignUp;
