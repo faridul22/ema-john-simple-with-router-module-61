305af42b2ca4d19fbda569b3a634edfad8dbdb21
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-01-30 11:09:17 +0600

    login page basic setUp complete

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,8 +3,21 @@ import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login coming sooon</h2>
+        <div className='form_container'>
+            <h2 className='form_title'>Login</h2>
+            <form>
+                <div className="form_control">
+                    <label htmlFor="">Email</label>
+                    <input type="email" name="email" id="" required />
+                </div>
+                <div className="form_control">
+                    <label htmlFor="">Password</label>
+                    <input type="password" name="password" id="" required />
+                </div>
+                <div className='form_control'>
+                    <input className='btn_submit' type="submit" value="Login" />
+                </div>
+            </form>
         </div>
     );
 };
